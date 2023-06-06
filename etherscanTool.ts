8dfc76c0e8ba776f3dc5fc07b406a27ee705ccc1
Srujan Gurram <srujangs8@gmail.com>
2023-06-06 16:33:31 +0530

    Refactor EtherscanTool.ts to include a utility function for creating the API url and update the code accordingly

--- etherscanTool.ts
+++ etherscanTool.ts
@@ -2,21 +2,36 @@ import { Tool } from "langchain/tools";
 import axios from "axios";
 
 export class EtherscanTransactionDetails extends Tool {
-    name = "etherscan-transaction-details";
-    description = "Use this tool if you want to get the transaction details of a transaction hash";
+  name = "etherscan-transaction-details";
+  description =
+    "Use this tool if you want to get the transaction details of a transaction hash";
 
-    async _call(input: string): Promise<string>{
-        const url = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${input}&apikey=IF9GS3DP38DRQABR5KH5V7ZUZUSIJRH8BE`;
-        const {data} = await axios.get(url);
+  async _call(input: string): Promise<string> {
+    const { data } = await axios.get(makeUrl({action: "eth_getTransactionByHash", txhash: input}))
+    
 
-        console.log(data)
+    console.log(data);
 
-        if(data.status == 0){
-            return `The transaction hash ${input} is not valid.`
-        }
-
-        const transaction = data.result;
-
-        return `The result for transaction hash ${input} is: \n ${JSON.stringify(transaction, null, 2)} \n convert hex units to decimal and format it in clean sentence before giving output to user.`
+    if (data.status == 0) {
+      return `The transaction hash ${input} is not valid.`;
     }
+
+    const transaction = data.result;
+
+    return `The result for transaction hash ${input} is: \n ${JSON.stringify(
+      transaction,
+      null,
+      2
+    )} \n convert hex units to decimal and format it in clean sentence before giving output to user.`;
+  }
 }
+
+/*============= UTILITY FUNCTIONS ===============*/
+
+const makeUrl = (params: Record<string, string>) => {
+  const url = new URL("https://api.etherscan.io/api");
+  const searchParams = {...params, module: "proxy", apikey: process.env.ETHERSCAN_API_KEY!}
+  url.search = new URLSearchParams(searchParams).toString();
+  console.log(url.toString())
+  return url.toString();
+};
