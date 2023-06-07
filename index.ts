66fc1a20bbf24caae2914ca127dbbfb660a27417
Srujan Gurram <srujangs8@gmail.com>
2023-06-08 04:54:58 +0530

    made querying docs working

--- index.ts
+++ index.ts
@@ -4,22 +4,22 @@ import { OpenAI } from "langchain/llms/openai";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import "dotenv/config";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 const main = async () => {
   // Create the models and chain
   const embeddings = new OpenAIEmbeddings();
-  const model = new OpenAI({
-    temperature: 0,
-  });
+  const model = new OpenAI({temperature: 0});
   const chain = loadQARefineChain(model);
 
   // Load the documents and create the vector store
   const loader = new TextLoader("./input.md");
-  const docs = await loader.load();
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await loader.loadAndSplit(textSplitter);
 
   const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
   // Select the relevant documents
-  const question = "How to build a query ?";
+  const question = "How to build a query to get latest transaction on ethereum ?";
   const relevantDocs = await store.similaritySearch(question);
 
   // Call the chain
