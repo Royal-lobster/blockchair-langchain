aa4e0c0b20486ab5c8d770c8cc3fcf950c0ece18
Yadheedhya.i <79125868+Yadheedhya06@users.noreply.github.com>
2023-07-08 23:48:18 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ class CustomRequestGetTool extends RequestsGetTool {
   async call(input: string) {
     try {
       const { data } = await axios.get(input);
-      // if any nested array or object have more than 50 elements, remove that key from data 
+      // if any nested array or object have more than 50 elements, remove that key from data bebeadadbaf
       const recurse = (obj: any) => {
         if (!obj) return;
         Object.keys(obj).forEach((key) => {
